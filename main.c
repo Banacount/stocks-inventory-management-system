277ce3816b7d7ad105dfe122991a4fe0b7dc28af
Banacount <uniquejmal@gmail.com>
2025-11-28 17:25:37 +0800

    I changed my approach frfr

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 #include <time.h>
 #include <conio.h>
 #include <Windows.h>
+#include <stdbool.h>
 
+#define INV_FILE "inventory.inv"
 //Global variables
 int loginCount = 4;
 
@@ -19,6 +21,7 @@ typedef struct {
   char item_name[50];
   int item_quantity;
   float item_price;
+  bool isExist;
 } Inventory;
 
 typedef struct {
@@ -29,6 +32,7 @@ typedef struct {
   float product_discount;
   float total_value;
   float commission_total;
+  bool isExist;
 } Sales;
 
 //Functions
@@ -38,14 +42,16 @@ void encrypt(char *text, int seed);
 void getInput(char *output_dest, size_t output_size);
 void getInputHidden(char *output_dest, size_t output_size);
 void getInputValidChars(char *output_dest, size_t output_size);
+void checkInvFile();
 void registering();
 void login(char *file_name);
 void clearScr();
-void inventoryMenu(FILE *file_pointer);
+void inventoryMenu();
 void disAssemInv(FILE *file_pointer);
-void addInventory(FILE *file_pointer);
+void addInventory(int item_id);
+void deleteInv();
 void invUIHead();
-void invUIlist(Inventory inventory, int isLast);
+void invUIlist(Inventory inventory);
 
 int main(int arg_count, char *args[]){
   UINT oldcon = GetConsoleOutputCP(); SetConsoleOutputCP(CP_UTF8);
@@ -162,13 +168,19 @@ void getInputOnlyNum(char *output_dest, size_t output_size){
     }
     output_dest[str_count] = '\0';
   }
-
-  return;
+}
+void checkInvFile(){
+  FILE *f = fopen(INV_FILE, "rb");
+  if(f == NULL){
+    f = fopen(INV_FILE, "wb");
+    fclose(f);
+    return;
+  }
 }
 
 //Register stock process
 void registering(){
-  int inv_count = 0, sales_count = 0;
+  int sales_count = 0;
   stock account;
   char init_txt[20]; char file_name[30];
   printf("Enter File Name: ");
@@ -189,7 +201,6 @@ void registering(){
     return;
   }
   fwrite(&account, sizeof(account), 1,f);
-  fwrite(&inv_count, sizeof(inv_count), 1,f);
   fwrite(&sales_count, sizeof(sales_count), 1,f);
   fclose(f);
   printf("Register process done!\n");
@@ -221,7 +232,7 @@ void login(char *file_name){
   int isUser = strcmp(account.userName, cmp.userName);
   int isPass = strcmp(account.userPassword, cmp.userPassword);
   if(isUser == 0 && isPass == 0){
-    inventoryMenu(f);
+    inventoryMenu();
   } else if(loginCount > 0){
     clearScr();
     loginCount--;
@@ -253,67 +264,113 @@ void encrypt(char *text, int seed){
   }
 }
 
-void addInventory(FILE *file_pointer){
+//Inventory methods
+void deleteInv(){
+  int id_to_delete; char id_buff[7]; char choose[1];
+  printf("Put the item ID: ");
+  getInputOnlyNum(id_buff, sizeof(id_buff));
+  printf("\n");
+  id_to_delete = atoi(id_buff);
+  
+  FILE *f = fopen(INV_FILE, "rb+");
+  if(f == NULL){
+    printf("Error opening file.\n");
+    return;
+  }
+
+  Inventory loop;
+  while(fread(&loop, sizeof(loop), 1, f)){
+    if(id_to_delete == loop.item_id){
+      printf("\033[92m");
+      printf("Data found: %d, %s\n", loop.item_id, loop.item_name);
+      printf("[ ] Type 1_if(yes) / 0_if(no)\r[");
+      getInputOnlyNum(choose, sizeof(choose));
+      if(atoi(choose) == 1){
+        loop.isExist = false;
+        fseek(f, sizeof(loop)*-1, SEEK_CUR);
+        fwrite(&loop, sizeof(loop), 1, f);
+        printf("\nDeleted!\n");
+        Sleep(1000);
+        break;
+      }
+    }
+  }
+  printf("\033[0m");
+  fclose(f);
+}
+void addInventory(int item_id){
+  FILE *f = fopen(INV_FILE, "ab");
+  if(f == NULL){
+    printf("Error opening file.\n");
+    return;
+  }
+
   Inventory to_add;
-  int inv_count, sale_count;
+  int sale_count;
 
-  fseek(file_pointer, sizeof(stock), SEEK_SET);
-  fread(&inv_count, sizeof(inv_count), 1, file_pointer);
-  fread(&sale_count, sizeof(sale_count), 1, file_pointer);
-
-  int new_inv_count = inv_count + 1;
-  char priceBuff[100], quantBuff[80];
-  to_add.item_id = new_inv_count;
+  char priceBuff[14], quantBuff[7];
+  to_add.item_id = item_id;
   printf("\033[92mEnter Item Name: \033[0m");
   getInputValidChars(to_add.item_name, sizeof(to_add.item_name));
-  printf("\033[92m\nEnter Item Price: \033[0m");
-  getInputOnlyNum(priceBuff, sizeof(priceBuff));
   printf("\033[92m\nEnter Item Quantity: \033[0m");
   getInputOnlyNum(quantBuff, sizeof(quantBuff));
+  printf("\033[92m\nEnter Item Price: \033[0m");
+  getInputOnlyNum(priceBuff, sizeof(priceBuff));
   to_add.item_price = atof(priceBuff);
   to_add.item_quantity = atoi(quantBuff);
-  long write_pos = sizeof(stock) + (sizeof(int) * 2) + (sizeof(Inventory) * (long)inv_count);
+  to_add.isExist = true;
 
-  fseek(file_pointer, write_pos, SEEK_SET);
-  fwrite(&to_add, sizeof(to_add), 1, file_pointer);
-  fseek(file_pointer, sizeof(stock), SEEK_SET);
-  fwrite(&new_inv_count, sizeof(new_inv_count), 1, file_pointer);
+  fwrite(&to_add, sizeof(to_add), 1, f);
+  fclose(f);
 }
 //Inventory creation menu
-void inventoryMenu(FILE *file_pointer){
-  int inv_count, sale_count;
+void inventoryMenu(){
+  int sale_count, end_of_inv;
   Inventory inv;
   int ch;
   while(1){
-    fseek(file_pointer, sizeof(stock), SEEK_SET);
-    fread(&inv_count, sizeof(inv_count), 1, file_pointer);
-    fread(&sale_count, sizeof(sale_count), 1, file_pointer);
+    fflush(stdin);
     clearScr();
     invUIHead();
-    for(int i = 0; i < inv_count; i++){
-      fread(&inv, sizeof(inv), 1, file_pointer);
-      if(i == inv_count-1) invUIlist(inv, 1);
-      else invUIlist(inv, 0);
+    checkInvFile();
+    //Get the inv count
+    end_of_inv = 0;
+    FILE *f = fopen(INV_FILE, "rb");
+    if(f == NULL){ printf("Error opening file.\n"); return; }
+    while(fread(&inv, sizeof(inv), 1, f)) end_of_inv++;
+
+    fseek(f, 0, SEEK_SET);
+    for(int i = 0; i < end_of_inv; i++){
+      fread(&inv, sizeof(inv), 1, f);
+      if(inv.isExist) invUIlist(inv);
     }
-    printf("Total Items [ %d ]\n", inv_count);
+    printf("└──────────────┴─────────────────────┴───────────────┴───────────────────┘\n\n");
+    fclose(f);
+    
+    printf("Total Items [ %d ]\n", end_of_inv);
     printf("[ D ] Delete an inventory item. \n");
     printf("[ A ] Add an inventory item. \n");
     printf("[ U ] Update details of an inventory item. \n");
+    printf("[ R ] To refresh. \n");
     printf("[ Esc or Enter ] To exit. \n");
     ch = _getch();
     if(ch == 27 || ch == 13){ printf("Exiting salamat sa pag gamit!\n"); break; }
     switch((char)ch){
       case 'd':
-        printf("Delete inventory option\n");
+        deleteInv();
         break;
       case 'a':
-        addInventory(file_pointer);
+        addInventory(end_of_inv);
         break;
       case 'u':
         printf("Update inventory option\n");
         break;
       case 's':
         printf("Search option\n");
+        break;
+      case 'r':
+        printf("Refresh\n");
+        Sleep(500);
         break;
       default:
         printf("Invalid option!\n");
@@ -326,18 +383,20 @@ void invUIHead(){
   printf("│ Item Code    │ Item Name           │ Item Quantity │ Item Price        │\n");
   printf("├──────────────┼─────────────────────┼───────────────┼───────────────────┤\n");
 };
-void invUIlist(Inventory inventory, int isLast){
+void invUIlist(Inventory inventory){
+  //Buffers
+  char id[13], quan[14], price[18];
+
   printf("│              │                     │               │                   │");
   printf("\r\033[2C");
-  printf("%d", inventory.item_id);
+  sprintf_s(id, sizeof(id), "%d", inventory.item_id); printf("%s", id);
   printf("\r\033[17C");
-  printf("%s", inventory.item_name);
+  printf("%.19s", inventory.item_name);
   printf("\r\033[39C");
-  printf("%d", inventory.item_quantity);
+  sprintf_s(quan, sizeof(quan), "%d", inventory.item_quantity); printf("%s", quan);
   printf("\r\033[56C");
-  printf("%.2f", inventory.item_price);
+  sprintf_s(price, sizeof(price), "%.2f", inventory.item_price); printf("%s", price);
   printf("\033[1B\r");
-  if(isLast == 0) printf("├──────────────┼─────────────────────┼───────────────┼───────────────────┤");
-  else printf("└──────────────┴─────────────────────┴───────────────┴───────────────────┘");
+  printf("├──────────────┼─────────────────────┼───────────────┼───────────────────┤");
   printf("\033[1B\r");
 }
